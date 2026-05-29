3dc4285c31cfc25543868607ba6b68cbeb0af8fd
Michael Agbozo <agbozomykell8@gmail.com>
2026-05-29 15:02:02 +0000

    fix: use 777 permissions for storage and bootstrap/cache

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ mkdir -p /var/log/nginx
 
 php-fpm -y /assets/php-fpm.conf -D
 
-chmod -R 775 /app/storage /app/bootstrap/cache
+chmod -R 777 /app/storage /app/bootstrap/cache
 
 php artisan storage:link --force
 php artisan config:cache
